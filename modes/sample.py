8f6962bb4ddca0bce338a5c185da8ae4faf120b1
James Prince <jamesprinc3@me.com>
2018-05-29 16:27:12 +0100

    Split market buy and sell limit/market orders wrt the distributions fitted.

--- modes/sample.py
+++ modes/sample.py
@@ -17,7 +17,6 @@ class Sample:
         correlations = {}
 
         with pebble.ProcessPool() as pool:
-
             price_size_corrs = Correlations.get_price_size_corr(trades_df, DataSplitter.get_limit_orders(orders_df))
             correlations['buy_price_size'] = price_size_corrs['buy']
             correlations['sell_price_size'] = price_size_corrs['sell']
@@ -27,8 +26,6 @@ class Sample:
                                                                (trades_df, orders_df,),
                                                                dict(relative=True))
 
-
-
             # Buy/sell Price
             order_price_distributions = pool.schedule(DataTransformer.price_distributions,
                                                       (trades_df, orders_df,), dict(relative=False))
@@ -37,14 +34,26 @@ class Sample:
             relative_cancel_price_distributions = pool.schedule(DataTransformer.price_distributions,
                                                                 (trades_df, cancels_df,))
 
-            # Limit/ Market Order Size
+            # Limit Order Size
             limit_orders = DataSplitter.get_limit_orders(orders_df)
-            limit_size = pool.schedule(DistributionFitter.best_fit_distribution,
-                                       (limit_orders['size'],))
+            buy_limit_orders = DataSplitter.get_side("buy", limit_orders)
+            sell_limit_orders = DataSplitter.get_side("sell", limit_orders)
+
+            buy_limit_size = pool.schedule(DistributionFitter.best_fit_distribution,
+                                           (buy_limit_orders['size'],))
+            sell_limit_size = pool.schedule(DistributionFitter.best_fit_distribution,
+                                            (sell_limit_orders['size'],))
+
+            # Market Order Size
 
             market_orders = DataSplitter.get_market_orders(orders_df)
-            market_size = pool.schedule(DistributionFitter.best_fit_distribution,
-                                        (market_orders['size'],))
+            buy_market_orders = DataSplitter.get_side("buy", market_orders)
+            sell_market_orders = DataSplitter.get_side("sell", market_orders)
+
+            buy_market_size = pool.schedule(DistributionFitter.best_fit_distribution,
+                                           (buy_market_orders['size'],))
+            sell_market_size = pool.schedule(DistributionFitter.best_fit_distribution,
+                                            (sell_market_orders['size'],))
 
             intervals = pool.schedule(DataTransformer.intervals_distribution, (orders_df,))
 
@@ -63,19 +72,26 @@ class Sample:
             distributions["buy_cancel_price"] = relative_cancel_price_distributions.result()["buy"][1]
             distributions["sell_cancel_price"] = relative_cancel_price_distributions.result()["sell"][1]
 
-            limit_size_best_fit, limit_size_best_fit_params = limit_size.result()
-            _, distributions["limit_size"] = DistributionFitter.get_distribution_string(limit_size_best_fit,
-                                                                                        limit_size_best_fit_params)
+            buy_limit_size_best_fit, buy_limit_size_best_fit_params = buy_limit_size.result()
+            _, distributions["buy_limit_size"] = DistributionFitter.get_distribution_string(buy_limit_size_best_fit,
+                                                                                            buy_limit_size_best_fit_params)
 
-            market_size_best_fit, market_size_best_fit_params = market_size.result()
-            _, distributions["market_size"] = DistributionFitter.get_distribution_string(market_size_best_fit,
-                                                                                         market_size_best_fit_params)
+            sell_limit_size_best_fit, sell_limit_size_best_fit_params = sell_limit_size.result()
+            _, distributions["sell_limit_size"] = DistributionFitter.get_distribution_string(sell_limit_size_best_fit,
+                                                                                             sell_limit_size_best_fit_params)
+
+            buy_market_size_best_fit, buy_market_size_best_fit_params = buy_market_size.result()
+            _, distributions["buy_market_size"] = DistributionFitter.get_distribution_string(buy_market_size_best_fit,
+                                                                                             buy_market_size_best_fit_params)
+
+            sell_market_size_best_fit, sell_market_size_best_fit_params = sell_market_size.result()
+            _, distributions["sell_market_size"] = DistributionFitter.get_distribution_string(sell_market_size_best_fit,
+                                                                                              sell_market_size_best_fit_params)
 
             _, distributions["interval"] = intervals.result()
 
             params['ratios'] = ratios
             params['correlations'] = correlations
             params['distributions'] = distributions
-
 
         return params
